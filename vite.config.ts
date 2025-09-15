9523bec700c38f04505cbce0e916515bc8a6b41a
RileyManda <rileymanda0@gmail.com>
2025-09-15 23:34:38 +0200

    chore:Added Vitest setup and configuration for testing environment
    - Created a new setup file for Vitest that mocks global DOM elements.
    - Updated Vite configuration to include test settings, specifying jsdom as the environment and adding the setup file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
     emptyOutDir: true,
     rollupOptions: {
-      input: 'index.html',
+      input: "index.html",
     },
   },
   server: {
     port: 3000,
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/test/setup.ts"],
   },
 });
