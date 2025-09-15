466ab0e5674f2f80fc3e54ab4b0ee75d9481768a
RileyManda <rileymanda0@gmail.com>
2025-09-15 23:35:28 +0200

    feat: Added initial test for Header component using Vitest to verfy test configs

---
+++ src/test/Header.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("Header Component", () => {
+  it("should pass basic test to verify setup", () => {
+    expect(true).toBe(true);
+  });
+});
