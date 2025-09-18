f7ad176e7742eaeb69a7b281af68f2dda94ad5fc
RileyManda <rileymanda0@gmail.com>
2025-09-19 00:52:29 +0200

    feat: Addedtype annotation for user state in global state management

--- src/components/global/globalState.ts
+++ src/components/global/globalState.ts
@@ -1,8 +1,10 @@
 import { signal } from "sigment";
+import type { User } from "../../types";
+
 export const [userName, setUserName] = signal("");
 export const [userMail, setUserMail] = signal("");
 export const [isAuthenticated, setIsAuthenticated] = signal(false);
-export const [user, setUser] = signal(null);
+export const [user, setUser] = signal<User | null>(null);
 export const [authToken, setAuthToken] = signal("");
 export const [showLoginForm, setShowLoginForm] = signal(false);
 export const [showCreateArticleForm, setShowCreateArticleForm] = signal(false);
